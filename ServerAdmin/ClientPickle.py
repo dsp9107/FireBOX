df111f1eeb286cde1f22df4b7b6b555d693867a7
Dhawal Singh Panwar <dsp9107@outlook.com>
2019-09-12 23:36:00 +0530

    Code Stripped Down To Necessities

--- ServerAdmin/ClientPickle.py
+++ ServerAdmin/ClientPickle.py
@@ -9,31 +9,33 @@ def prep(msg):
     payload = bytes(str(len(msg)).ljust(HEADERSIZE),"utf-8") + payload
     return payload
 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.137.7', 1237))
+s.connect((socket.gethostname(), 1237))
 
 while True:
     print("CONNECTED")
     full_msg = b''
     new_msg = True
     while True:
-        msg = s.recv(16)
+        msg = s.recv(1024)
         if new_msg:
-            print("RECEIVED")
-            print(f"Message Length : {msg[:HEADERSIZE]}")
+#            print("RECEIVED")
+#            print(f"Message Length : {msg[:HEADERSIZE]}")
             msglen = int(msg[:HEADERSIZE].decode())
             new_msg = False
-        full_msg += msg
+        full_msg += msg[HEADERSIZE:]
         if(len(full_msg)-HEADERSIZE == msglen):
-           print("Full Message Received")
-           print(full_msg[HEADERSIZE:])
+#           print("Full Message Received")
+#           print(full_msg[HEADERSIZE:])
 
-           d = pickle.loads(full_msg[HEADERSIZE:])
+           d = pickle.loads(full_msg)
            print(d)
            
            new_msg = True
            full_msg = b''
            print()
-
-           s.send(prep("HellO BaCK"))
-           print("SENT")
+        while True:
+            mess = str(input("SERVER : "))
+            s.send(prep(mess))
+#            print("SENT")
