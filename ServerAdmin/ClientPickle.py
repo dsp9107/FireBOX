b8e8c6e0d9d904aecb5bca1a05658f22dc558261
Dhawal Singh Panwar <dsp9107@outlook.com>
2019-09-12 16:16:28 +0530

    Update ClientPickle.py

--- ServerAdmin/ClientPickle.py
+++ ServerAdmin/ClientPickle.py
@@ -4,21 +4,28 @@ import pickle
 
 HEADERSIZE = 10
 
+def prep(msg):
+    payload = pickle.dumps(msg)
+    payload = bytes(str(len(msg)).ljust(HEADERSIZE),"utf-8") + payload
+    return payload
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1236))
+s.connect(('192.168.137.7', 1237))
 
 while True:
+    print("CONNECTED")
     full_msg = b''
     new_msg = True
     while True:
         msg = s.recv(16)
         if new_msg:
-            print(f"new message length : {msg[:HEADERSIZE]}")
-            msglen = int(msg[:HEADERSIZE])
+            print("RECEIVED")
+            print(f"Message Length : {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE].decode())
             new_msg = False
         full_msg += msg
         if(len(full_msg)-HEADERSIZE == msglen):
-           print("full message recvd")
+           print("Full Message Received")
            print(full_msg[HEADERSIZE:])
 
            d = pickle.loads(full_msg[HEADERSIZE:])
@@ -26,3 +33,7 @@ while True:
            
            new_msg = True
            full_msg = b''
+           print()
+
+           s.send(prep("HellO BaCK"))
+           print("SENT")
