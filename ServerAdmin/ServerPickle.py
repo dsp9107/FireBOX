d8b605208c008bac3376cd45ab4747194f184318
Dhawal Singh Panwar <dsp9107@outlook.com>
2019-09-12 16:17:02 +0530

    Update ServerPickle.py

--- ServerAdmin/ServerPickle.py
+++ ServerAdmin/ServerPickle.py
@@ -3,7 +3,13 @@ import time
 import pickle
 
 HEADERSIZE = 10
-d = {1: "Hello", 2: "World"}
+
+def prep(msg):
+    payload = pickle.dumps(msg)
+    payload = bytes(str(len(msg)).ljust(HEADERSIZE),"utf-8") + payload
+    return payload
+
+msg = {1: "Hello", 2: "World"}
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 1236))
 s.listen(5)
@@ -12,7 +18,5 @@ while True:
     ClientSocket, Address = s.accept()
     print(f"Connection from {Address} has been established")
     msg = pickle.dumps(d)
-    #msg = bytes(f'{len(msg):<{HEADERSIZE}}', "utf-8") + msg
-    msg = bytes(str(len(msg)).ljust(HEADERSIZE),"utf-8") + msg
 
-    ClientSocket.send(msg)
+    ClientSocket.send(prep(msg))
