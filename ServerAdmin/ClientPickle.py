6b0f78dcf8c6b1f42e6a801023e26228a2529404
Dhawal Singh Panwar <dsp9107@outlook.com>
2019-09-14 19:22:01 +0530

    Update ClientPickle.py

--- ServerAdmin/ClientPickle.py
+++ ServerAdmin/ClientPickle.py
@@ -1,50 +1,52 @@
+import json
 import socket
-import time
-import pickle
+import jsontransport as jt
 
-HEADERSIZE = 10
-ENCODING = "utf-8"
-reqexit = {'terminate': 1}
-reqhn = {'hostname': 1}
-reqpubkey = {'pubKey': 1}
+def main():
+    global config, host, s
 
-def pack(msg):
-    payload = pickle.dumps(msg)
-    payload = bytes(str(len(msg)).ljust(HEADERSIZE), ENCODING) + payload
-    return payload
+    with open("config.json", "r") as read_file:
+        config = json.load(read_file)
 
-def unpack(message):
-    msglen = int(message[:HEADERSIZE].decode())
-    full_msg = message[HEADERSIZE:]
-    if(len(full_msg)-HEADERSIZE == msglen):
-        msg = pickle.loads(full_msg)
-    return msg
+    config['reqtype'] = {i : 0 for i in config['reqtype']}
+    host = 'localhost'
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = 'localhost'
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if __name__ == '__main__' :
+    main()
+
 s.connect((host, 1237))
-
 print(f"\nConnection With {host} Has Been Established")
 
-message = unpack(s.recv(1024))
+message = jt.unpack(s.recv(1024), config['HEADERSIZE'])['messageContent']
 print("SERVER ~ " + message)
+
 while True:
     mess = str(input("SERVER : "))
+
     if mess == "<reqhn>" :
-        mess = str(reqhn)
-        s.send(pack(mess))
+        mess = config['reqtype']
+        mess['hostname']=1
+        s.send(jt.pack(jt.prep(mess, "request"), config['HEADERSIZE']))
         hn = s.recv(1024)
-        print("SERVER ~ " + unpack(hn))
-    elif mess == "<reqpubkey>" :
-        mess = str(reqpubkey)
-        s.send(pack(mess))
+        print("SERVER ~ " + jt.unpack(hn, config['HEADERSIZE'])['messageContent'])
+
+    elif mess == "<reqpk>" :
+        mess = config['reqtype']
+        mess['pubKey']=1
+        s.send(jt.pack(jt.prep(mess, "request"), config['HEADERSIZE']))
         hn = s.recv(1024)
-        print("SERVER ~ " + unpack(hn))
-    elif mess == "<reqexit>" :
-        mess = str(reqexit)
-        s.send(pack(mess))
+        print("SERVER ~ " + jt.unpack(hn, config['HEADERSIZE'])['messageContent'])
+
+    elif mess == "<exit>" :
+        mess = config['reqtype']
+        mess['terminate']=1
+        s.send(jt.pack(jt.prep(mess, "request"), config['HEADERSIZE']))
         break
+
     else :
-        s.send(pack(mess))
-            
+        s.send(jt.pack(jt.prep(mess), config['HEADERSIZE']))
+
+    config['reqtype'] = {i : 0 for i in config['reqtype']}
+        
 print(f"Connection With {host} Has Been Terminated")
