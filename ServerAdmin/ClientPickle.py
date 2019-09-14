246897d919dd6a64ec6cab575240fb17b6771cd4
Dhawal Singh Panwar <dsp9107@outlook.com>
2019-09-14 12:21:45 +0530

    Compartmentalized Thus Simplified

--- ServerAdmin/ClientPickle.py
+++ ServerAdmin/ClientPickle.py
@@ -3,39 +3,43 @@ import time
 import pickle
 
 HEADERSIZE = 10
+ENCODING = "utf-8"
+reqhn = {'request': [{'hostname': 1}]}
 
-def prep(msg):
+def pack(msg):
     payload = pickle.dumps(msg)
-    payload = bytes(str(len(msg)).ljust(HEADERSIZE),"utf-8") + payload
+    payload = bytes(str(len(msg)).ljust(HEADERSIZE), ENCODING) + payload
     return payload
 
+def unpack(message):
+    msglen = int(message[:HEADERSIZE].decode())
+    full_msg = message[HEADERSIZE:]
+    if(len(full_msg)-HEADERSIZE == msglen):
+        msg = pickle.loads(full_msg)
+    return msg
 
+host = 'localhost'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1237))
+s.connect((host, 1237))
+
+print(f"\nConnection With {host} Has Been Established")
 
 while True:
-    print("CONNECTED")
-    full_msg = b''
-    new_msg = True
+    msg = s.recv(1024)
+    if msg:
+        message = unpack(msg)
+    print("SERVER ~ " + message)
     while True:
-        msg = s.recv(1024)
-        if new_msg:
-#            print("RECEIVED")
-#            print(f"Message Length : {msg[:HEADERSIZE]}")
-            msglen = int(msg[:HEADERSIZE].decode())
-            new_msg = False
-        full_msg += msg[HEADERSIZE:]
-        if(len(full_msg)-HEADERSIZE == msglen):
-#           print("Full Message Received")
-#           print(full_msg[HEADERSIZE:])
-
-           d = pickle.loads(full_msg)
-           print(d)
-           
-           new_msg = True
-           full_msg = b''
-           print()
-        while True:
-            mess = str(input("SERVER : "))
-            s.send(prep(mess))
-#            print("SENT")
+        mess = str(input("SERVER : "))
+        if mess == "<reqhn>" :
+            mess = str(reqhn)
+            s.send(pack(mess))
+            hn = s.recv(1024)
+            print("SERVER ~ " + unpack(hn))
+        elif mess == "<exit>" :
+            s.send(pack(mess))
+            exit()
+        else :
+            s.send(pack(mess))
+            
+print("Connection Terminated")
