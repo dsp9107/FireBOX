d762192e105d1d03115a1853b06d4482ce2f3aba
chiragseda <36998229+chiragseda@users.noreply.github.com>
2019-10-20 13:06:59 +0530

    Add files via upload

---
+++ ClientSide/listview_custom.java
@@ -0,0 +1,14 @@
+package com.example.wifi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listview_custom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listview_custom);
+    }
+}
