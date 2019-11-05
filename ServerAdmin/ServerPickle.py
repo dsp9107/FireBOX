810429bf3ba84cc7c9f805fd87517817da845567
dsp9107 <dsp9107@outlook.com>
2019-11-05 22:07:44 +0530

    Exception Handling For JSON Decode Error

--- ServerAdmin/ServerPickle.py
+++ ServerAdmin/ServerPickle.py
@@ -54,83 +54,94 @@ while True:
 
     else :
         # When Connected
-        message = jt.unpack(ClientSocket.recv(1024), config['headerSize'])
+        msg = ClientSocket.recv(1024)
 
-        # If Message Is Login Request
-        if message['messageType'] == "login" :
-            user = {"uname": "", "pwd": ""}
-            user['uname'] = message['messageContent']['uname']
-            user['pwd'] = th.threeHash(message['messageContent']['pwd'])
-            # Load DB
-            with open("users.json", "r") as read_file :
-                users = json.load(read_file)
-            # If User Registered
-            if user['uname'] in users :
-                # If Passwords Match
-                if user['pwd'] == users[user['uname']] :
-                    # Acknowledge
-                    ClientSocket.send(jt.pack(jt.prep(f"Welcome, {user['uname']}"), config['headerSize']))
-                    print(f"\nConnection From {Address} Has Been Established")
+        try :
+            # Try Unpacking The Message
+            message = jt.unpack(msg, config['headerSize'])
+
+        except json.decoder.JSONDecodeError :
+            # If JSON Decoding Error
+            print("Improper Format Used")
+            s.close()
+            sys.exit()
+
+        else :
+            # If Message Is Login Request
+            if message['messageType'] == "login" :
+                user = {"uname": "", "pwd": ""}
+                user['uname'] = message['messageContent']['uname']
+                user['pwd'] = th.threeHash(message['messageContent']['pwd'])
+                # Load DB
+                with open("users.json", "r") as read_file :
+                    users = json.load(read_file)
+                # If User Registered
+                if user['uname'] in users :
+                    # If Passwords Match
+                    if user['pwd'] == users[user['uname']] :
+                        # Acknowledge
+                        ClientSocket.send(jt.pack(jt.prep(f"Welcome, {user['uname']}"), config['headerSize']))
+                        print(f"\nConnection From {Address} Has Been Established")
+                    else :
+                        ClientSocket.send(jt.pack(jt.prep("Invalid User Details"), config['headerSize']))
+                        failed += 1
+                        print(f"Failed Attempt : {failed}")
+                        logUpdate(msg=f"{user['uname']} : Failed Login Attempt")
+                        ClientSocket.close()
+                        continue
                 else :
                     ClientSocket.send(jt.pack(jt.prep("Invalid User Details"), config['headerSize']))
-                    failed += 1
-                    print(f"Failed Attempt : {failed}")
-                    logUpdate(msg=f"{user['uname']} : Failed Login Attempt")
-                    ClientSocket.close()
                     continue
-            else :
-                ClientSocket.send(jt.pack(jt.prep("Invalid User Details"), config['headerSize']))
+
+            # If Message Is Registration Request
+            elif message['messageType'] == "register" :
+                user = {"uname": "", "pwd": ""}
+                user['uname'] = message['messageContent']['uname']
+                user['pwd'] = th.threeHash(message['messageContent']['pwd'])
+                # Load DB
+                with open("users.json", "r") as read_file :
+                    users = json.load(read_file)
+                users[user['uname']] = user['pwd']
+                # Update DB
+                with open("users.json", "w") as write_file :
+                    json.dump(users, write_file)
+                # Acknowledge
+                ClientSocket.send(jt.pack(jt.prep("Registration Successful"), config['headerSize']))
+                logUpdate(msg=f"{user['uname']} Has Been Registered")
+                print(f"{user['uname']} Has Registered")
+                ClientSocket.close()
                 continue
 
-        # If Message Is Registration Request
-        elif message['messageType'] == "register" :
-            user = {"uname": "", "pwd": ""}
-            user['uname'] = message['messageContent']['uname']
-            user['pwd'] = th.threeHash(message['messageContent']['pwd'])
-            # Load DB
-            with open("users.json", "r") as read_file :
-                users = json.load(read_file)
-            users[user['uname']] = user['pwd']
-            # Update DB
-            with open("users.json", "w") as write_file :
-                json.dump(users, write_file)
-            # Acknowledge
-            ClientSocket.send(jt.pack(jt.prep("Registration Successful"), config['headerSize']))
-            logUpdate(msg=f"{user['uname']} Has Been Registered")
-            print(f"{user['uname']} Has Registered")
-            ClientSocket.close()
-            continue
+            # If First Message Is Anything Else, Drop Connection
+            else :
+                ClientSocket.close()
+                continue
 
-        # If First Message Is Anything Else, Drop Connection
-        else :
-            ClientSocket.close()
-            continue
+            # Serve
+            while True:
+                # Receive Message
+                message = jt.unpack(ClientSocket.recv(1024), config['headerSize'])
 
-        # Serve
-        while True:
-            # Receive Message
-            message = jt.unpack(ClientSocket.recv(1024), config['headerSize'])
+                # Log Message
+                logUpdate(ip=Address[0], port=Address[1], user=user['uname'], msgtype=message['messageType'], msglen=message['messageLength'], msgcontent=message['messageContent'])
 
-            # Log Message
-            logUpdate(ip=Address[0], port=Address[1], user=user['uname'], msgtype=message['messageType'], msglen=message['messageLength'], msgcontent=message['messageContent'])
+                # If Client Sends A Request
+                if message['messageType'] == "request" :
+                    # If Client Requests To Disconnect
+                    if message['messageContent']['terminate'] :
+                        print(f"{user['uname']} : <exiting>")
+                        break
 
-            # If Client Sends A Request
-            if message['messageType'] == "request" :
-                # If Client Requests To Disconnect
-                if message['messageContent']['terminate'] :
-                    print(f"{user['uname']} : <exiting>")
-                    break
+                    # If Client Requests Public Key
+                    elif message['messageContent']['pubKey'] :
+                        print(f"{user['uname']} : <requesting publicKey>")
+                        # Send Public Key
+                        ClientSocket.send(jt.pack(jt.prep(pubKey), config['headerSize']))
 
-                # If Client Requests Public Key
-                elif message['messageContent']['pubKey'] :
-                    print(f"{user['uname']} : <requesting publicKey>")
-                    # Send Public Key
-                    ClientSocket.send(jt.pack(jt.prep(pubKey), config['headerSize']))
+                # If Client Sends A Message
+                elif message['messageType'] == "message" :
+                    print(f"{user['uname']} : " + message['messageContent'])
 
-            # If Client Sends A Message
-            elif message['messageType'] == "message" :
-                print(f"{user['uname']} : " + message['messageContent'])
-
-        logUpdate(msg=f"{user['uname']} Disconnected From {Address}")
-        print(f"{user['uname']} Has Left")
+            logUpdate(msg=f"{user['uname']} Disconnected From {Address}")
+            print(f"{user['uname']} Has Left")
 
