5881e36b0ffaae70920fd384b3ecd79be67f5b41
dsp9107 <dsp9107@outlook.com>
2019-11-12 21:53:31 +0530

    Update start.sh

--- Scripts/start.sh
+++ Scripts/start.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
 echo "Checking For Updates . . ."
+sleep 1
 sh update.sh
-echo "Initiating . . ."
 cd FireBOX_Sources/ServerAdmin
+sleep 3
+echo "Setting Up Firewall . . ."
+python3 ReConfigureFirewall.py
+echo "Waking Up chigfy To Obscure Traffic"
+nohup python3 TrendGetter.py &
+sleep 3
+echo "Ads-Delivery Services Are Unreachable"
+sleep 2
+echo "chigfy Is Ensuring Clients' Privacy"
+sleep 3
+echo "Initiating Authentication Server . . ."
+sleep 1
 python3 ServerPickle.py
