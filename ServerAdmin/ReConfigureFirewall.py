2c7da052bcedee7edd64e99c56fbb48ed5e11c8d
dsp9107 <dsp9107@outlook.com>
2019-11-12 21:35:00 +0530

    No User Interaction Needed

--- ServerAdmin/ReConfigureFirewall.py
+++ ServerAdmin/ReConfigureFirewall.py
@@ -3,7 +3,7 @@ import os
 
 if sys.platform == "linux" :
     os.system("sudo ufw disable")
-    os.system("sudo ufw reset")
+    os.system("sudo ufw reset --force")
     os.system("sudo ufw default reject incoming")
     os.system("sudo ufw default allow outgoing")
     os.system("sudo ufw default reject routed")
@@ -11,6 +11,6 @@ if sys.platform == "linux" :
     os.system("sudo ufw allow dns")
     os.system("sudo ufw allow 67")
     os.system("sudo ufw allow 9107")
-    os.system("sudo ufw enable")
+    os.system("sudo ufw --force enable")
 
 print("Firewall Configured")
